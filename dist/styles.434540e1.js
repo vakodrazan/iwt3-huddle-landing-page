028c301f84fad149e9e46759a38dde12204d158e
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-02-20 11:23:17 +0300

    Set the default style and the fonts as well

--- dist/styles.434540e1.js
+++ dist/styles.434540e1.js
@@ -184,7 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/index.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./..\\fonts\\open-sans-700.woff2":[["open-sans-700.dd697487.woff2","fonts/open-sans-700.woff2"],"fonts/open-sans-700.woff2"],"./..\\fonts\\open-sans-700.woff":[["open-sans-700.eb0c747e.woff","fonts/open-sans-700.woff"],"fonts/open-sans-700.woff"],"./..\\fonts\\open-sans-600.woff2":[["open-sans-600.309cc2ad.woff2","fonts/open-sans-600.woff2"],"fonts/open-sans-600.woff2"],"./..\\fonts\\open-sans-600.woff":[["open-sans-600.f5b6e316.woff","fonts/open-sans-600.woff"],"fonts/open-sans-600.woff"],"./..\\fonts\\open-sans-400.woff2":[["open-sans-400.215ce20d.woff2","fonts/open-sans-400.woff2"],"fonts/open-sans-400.woff2"],"./..\\fonts\\open-sans-400.woff":[["open-sans-400.e8c939c8.woff","fonts/open-sans-400.woff"],"fonts/open-sans-400.woff"],"./..\\fonts\\poppins-700.woff2":[["poppins-700.3e73bda6.woff2","fonts/poppins-700.woff2"],"fonts/poppins-700.woff2"],"./..\\fonts\\poppins-700.woff":[["poppins-700.facf664b.woff","fonts/poppins-700.woff"],"fonts/poppins-700.woff"],"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -212,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49791" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50125" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
